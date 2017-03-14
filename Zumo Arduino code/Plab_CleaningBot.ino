e535fc92c80d2566d988e92a7e995aa1460c3d87
plab-idi <terjero@idi.ntnu.no>
2017-03-14 10:20:42 +0100

    v2

--- Zumo Arduino code/Plab_CleaningBot.ino
+++ Zumo Arduino code/Plab_CleaningBot.ino
@@ -1,28 +1,4 @@
 /*
-  ProximitySensor
-  
-  Dette er et eksempel på hvordan du kan bruke en HC-SR04 ultralyd sensor med biblioteket NewPing for å måle avstand.
-  Lengde (i centimeter) måles og skrives ut hver 50 ms.
-  
-  Kretsen:
-    1 x HC-SR04 (ultralyd sensor)
-  Oppkobling vises på wiki:
-  https://www.ntnu.no/wiki/display/plab/3.+Ultralyd+-+HC-SR04
-  
-  Bibliotek
-    - NewPing (Nedlasting: https://code.google.com/p/arduino-new-ping/downloads/list )
-  
-  
-  This is an example on how you may use a HC-SR04 ultrasonic sensor with the library NewPing to detect distance.
-  Distance (in centimetres) is measured and printed every 50 ms.
-  
-  Circuit
-    1 x HC-SR04 (ultrasonic sensor)
-  How to connect circuit can be seen here:
-  https://www.ntnu.no/wiki/display/plab/3.+Ultralyd+-+HC-SR04
-  
-  Library
-    - NewPing (Download: https://code.google.com/p/arduino-new-ping/downloads/list )
  */
 #include <NewPing.h>
 #include <NewServo.h>
